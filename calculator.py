d8f29c034b2dff9236593867d03a9cd7def1e74a
Aleks <sandrolomadzegabiani@gmail.com>
2019-04-05 09:10:42 -0700

    minor update: memes, button_text
    Added text overlays to buttons to replace string commands.
    
    Added meme backgrounds.
    
    Still, the calculator is too functional for its own good. I need to add shuffle asap

--- calculator.py
+++ calculator.py
@@ -19,41 +19,51 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 300, 400
 first=None
 needClear=False
 
+meme_images=("surreal_feel.png","smile_4d_cancer.gif")
 win = GraphWin("Y E E T C U L A T O R", WINDOW_WIDTH, WINDOW_HEIGHT)
-UI= Image(Point(WINDOW_WIDTH/2, WINDOW_HEIGHT/2),"calculatorUI.png")
+MEME= Image(Point(WINDOW_WIDTH/2, WINDOW_HEIGHT/2),"background_memes/"+choice(meme_images))
+MEME.draw(win)
+UI= Image(Point(WINDOW_WIDTH/2, 50),"calculatorUI.gif")
 UI.draw(win)
-
 
 def buttons():
     button_list=[]
-    generated_button_commands=("1", "2","3" ,"4", "5", "6", "7", 
-                               "8", "9", "0", ".", "rand")
-    tmp=-1
-    num_buttons=[]
+    button_commands=("1", "2", "3", "4", "5", "6", "7", 
+                      "8", "9", "0", ".", "rand", "-",
+                      "+", "enter", "/", "<-", "X", "*") #need to be arranged to match correct buttons
+
+    generated_buttons=[]
     for i in range(4):
         for j in range(3):
-            tmp+=1
-            button_list.append((Rectangle(Point(75.0+(j*3)+(j-1)*63, 205.0+(i*3)+(i-1)*57), 
-                                           Point(75.0+(j*3)+(j)*63, 205.0+(i*3)+(i)*57))
-                                           ,generated_button_commands[tmp]))
+            generated_buttons.append(Rectangle(Point(75.0+(j*3)+(j-1)*63, 205.0+(i*3)+(i-1)*57), 
+                                           Point(75.0+(j*3)+(j)*63, 205.0+(i*3)+(i)*57)))
 
     cl = Rectangle(Point(17.0, 92.0), Point(71.0, 139.0)) # points are ordered ul, lr
-    di = Rectangle(Point(86.0, 94.0), Point(140.0, 137.0))
-    mu = Rectangle(Point(154.0, 93.0), Point(208.0, 139.0))
+    di = Rectangle(Point(86.0, 92.0), Point(140.0, 139.0))
+    mu = Rectangle(Point(154.0, 92.0), Point(208.0, 139.0))
+    ba = Rectangle(Point(223.0, 92.0), Point(278.0, 139.0))
     su = Rectangle(Point(223.0, 152.0), Point(278.0, 197.0))
     ad = Rectangle(Point(223.0, 206.0), Point(278.0, 251.0))
-    en = Rectangle(Point(222.0, 264.0), Point(275.0, 383.0))
-    ba = Rectangle(Point(223.0, 94.0), Point(278.0, 138.0))
+    en = Rectangle(Point(223.0, 264.0), Point(278.0, 383.0))
+    
 
+    hard_coded_buttons=[su, ad, en, di, ba, cl, mu]
     
-    hard_coded_buttons=[(su,"-"), (ad,"+"), (en,"enter"), (di, "/"), (ba, "<-"), (cl, "X"), (mu, "*")]
+    temp=-1
+    for button in generated_buttons:
+        temp+=1
+        button_list.append((button, Text(Point((button.getP1().getX()+button.getP2().getX())/2
+                   , (button.getP1().getY()+button.getP2().getY())/2), button_commands[temp])))
     
-    for hard_coded_button in hard_coded_buttons:
-        button_list.append(hard_coded_button)
-    
+    for button in hard_coded_buttons:
+        temp+=1
+        button_list.append((button, Text(Point((button.getP1().getX()+button.getP2().getX())/2
+                   , (button.getP1().getY()+button.getP2().getY())/2), button_commands[temp])))
         
     for button in button_list:
+        button[0].setFill("white")
         button[0].draw(win)
+        button[1].draw(win)
         
     return button_list
 
@@ -74,10 +84,11 @@ def action(button):
     
     if needClear==True:
         needClear=False
-        action(button_list[9])
+        action(button_list[17])
         
     if button!=None:
         refference, command =button
+        command=command.getText()
         if command== "1":
             text.setText(text.getText()+"1")
             return True
@@ -181,7 +192,7 @@ def action(button):
             second=text.getText()
             first1=first
             operation=textOperation.getText()
-            action(button_list[9])
+            action(button_list[17])
             if second!="":
                 second=float(second)
                 if operation=="/":
@@ -218,7 +229,7 @@ text = Text(Point(150, 50), "")
 text.setSize(28)
 text.draw(win)
 
-textOperation= Text(Point(16, 50), "")
+textOperation= Text(Point(25, 50), "")
 textOperation.setSize(28)
 textOperation.draw(win)
 
@@ -232,7 +243,7 @@ while cont:
     if clickPoint is not None:  # so we can substitute checkMouse() for getMouse()
         for button in button_list:
             if inside(clickPoint, button[0]):
-                print(button[1])
+                print(button[1].getText())
                 cont= action(button)
    
 
